2155fb37f60d99648471e4bc3fe6dd1cd8120085
Anshu Shukla <210305105002@paruluniversity.ac.in>
2024-05-30 10:08:56 +0530

    initial commit

---
+++ week-1/offline-class-1/level-2/01-Class.js
@@ -0,0 +1,15 @@
+
+class Animal {
+  constructor(name, legCount) {
+    this.name = name
+    this.legCount = legCount
+  }
+  describe() {
+    return `${this.name} has ${this.legCount} legs`
+  }
+}
+
+
+let obj = new Animal("Dog", 4);
+console.log(obj.describe());
+console.log(obj);
